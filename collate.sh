7f5f190c4df96dee369ebc4ddfbd206f746afdbb
Peter Kionga-Kamau <13005927+Mugane@users.noreply.github.com>
2025-01-16 12:46:58 -0500

    Create collate.sh
    Collate all registry files in ./registry into final complete registry fix file on github pages

---
+++ collate.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# This script collates the individual .reg templates in the ./registry folder into a single fixes.reg file
+# The output is published by GitHub Actions to GitHub Pages at https://mugane.github.io/Debonk-Windows-11/fixes.reg
+
+unlink ./fixes.reg 2>/dev/null # Quietly delete fixes.reg if it exists
+printf "{\n  \"version\": \"2\",\n  \"registry\": [\n" > ./fixes.reg
+for file in ./registry/*.reg; do # Collate all the .reg files in ./registry separated by two newlines
+  sed -e 's/^/    /'  -e 's/\\n\\s*$//' "$file" >> ./fixes.reg # Add spaces for formatting, remove trailing space/newlines
+  echo "\n\n" >> ./templates.json # Separate each joined registry file with two newlines
+done
+echo "./registry/ concatenated successfully into ./fixes.reg"

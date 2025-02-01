dd7083398ed4dbadefa586eff6b3ae4966a077f2
Peter Kionga-Kamau <13005927+Mugane@users.noreply.github.com>
2025-02-01 14:13:15 -0500

    Collate all registry code from files in ./registry into a single ./fixes.reg file for publication on Github Pages
    remove json characters and intentation from previously copied collation script

--- collate.sh
+++ collate.sh
@@ -4,9 +4,8 @@
 # The output is published by GitHub Actions to GitHub Pages at https://mugane.github.io/Debonk-Windows-11/fixes.reg
 
 unlink ./fixes.reg 2>/dev/null # Quietly delete fixes.reg if it exists
-printf "{\n  \"version\": \"2\",\n  \"registry\": [\n" > ./fixes.reg
 for file in ./registry/*.reg; do # Collate all the .reg files in ./registry separated by two newlines
-  sed -e 's/^/    /'  -e 's/\\n\\s*$//' "$file" >> ./fixes.reg # Add spaces for formatting, remove trailing space/newlines
+  sed -e 's/^//'  -e 's/\\n\\s*$//' "$file" >> ./fixes.reg # Add spaces for formatting, remove trailing space/newlines
   echo "\n\n" >> ./fixes.reg # Separate each joined registry file with two newlines
 done
 echo "./registry/ concatenated successfully into ./fixes.reg"
